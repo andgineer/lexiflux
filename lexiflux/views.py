8a2aa0ab58d974f8cd2044b03a7ecbaeb783970d
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2023-11-21 08:21:30 +0100

    page up

--- lexiflux/views.py
+++ lexiflux/views.py
@@ -8,21 +8,25 @@ from .models import BookPage
 
 def book(request: HttpRequest) -> HttpResponse:
     """Fetch paginated book and render only visible pages."""
-    page_number = request.GET.get("page", 1)
+    page_number = request.GET.get("book-page", 1)
     page = BookPage.objects.get(number=page_number)
-
+    # todo: get two pages for prev and next
     return render(request, "book.html", {"page": page})
 
 
 def book_page(request: HttpRequest) -> HttpResponse:
     """Render the book page."""
-    page_number = request.GET.get("page", 1)
+    page_number = request.GET.get("book-page", 1)
     try:
         next_page = BookPage.objects.get(number=page_number)
     except BookPage.DoesNotExist:
         return HttpResponse(f"error: Page {page_number} not found", status=500)
 
     # Render only the content of the next page as a partial HTML response
-    content = render_to_string("page.html", {"page": next_page})
+    content = render_to_string("page.html", {
+        "page": next_page,
+        "type": request.GET.get("type"),
+        "book_page": request.GET.get("book-page"),
+    })
 
     return HttpResponse(content)
