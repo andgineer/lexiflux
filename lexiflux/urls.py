e8dbff662abd53cf435bcd2ad9b49b350fddd8cc
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2023-11-23 15:39:00 +0100

    pinned bottom navbar word click event

--- lexiflux/urls.py
+++ lexiflux/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path("", views.book, name="book"),
     path("book-page", views.book_page, name="book-page"),
+    path("word", views.word_click, name="word"),
 ]
--- lexiflux/views.py
+++ lexiflux/views.py
@@ -1,13 +1,14 @@
 """Vies for the Lexiflux app."""
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.template import Context, Template
 from django.template.loader import render_to_string
 
 from .models import BookPage
 
 
 def book(request: HttpRequest) -> HttpResponse:
-    """Fetch paginated book and render only visible pages."""
+    """Render book."""
     page_number = request.GET.get("book-page", 1)
     page = BookPage.objects.get(number=page_number)
     words = page.content.split(" ")
@@ -33,11 +34,27 @@ def book_page(request: HttpRequest) -> HttpResponse:
     print("last_word_id", last_word_id)
     words = page.content.split(" ")
     content = render_to_string(
-        "book.html",
+        "page.html",
         {
             "page": page,
             "words": words,
         },
     )
 
+    return HttpResponse(content)
+
+
+def word_click(request: HttpRequest) -> HttpResponse:
+    """Word click event."""
+    page_number, word_number = request.GET.get("id", 1).split(":")
+    try:
+        page = BookPage.objects.get(number=page_number)
+    except BookPage.DoesNotExist:
+        return HttpResponse(f"error: Page {page_number} not found", status=500)
+    words = page.content.split(" ")
+    word = words[int(word_number) - 1]  # word_number is 1-based
+    print("word", page_number, word_number, word)
+    template = Template("<span id='word-{{ page.number }}:{{ word_id }}'>{{ word }}</span>")
+    context = Context({"word": word, "word_id": word_number})
+    content = template.render(context)
     return HttpResponse(content)
