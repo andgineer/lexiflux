6e26083300345d6b002492033f52a2e3a5ad9e06
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2023-11-23 10:33:12 +0100

    words visibility check

--- lexiflux/views.py
+++ lexiflux/views.py
@@ -10,25 +10,33 @@ def book(request: HttpRequest) -> HttpResponse:
     """Fetch paginated book and render only visible pages."""
     page_number = request.GET.get("book-page", 1)
     page = BookPage.objects.get(number=page_number)
-    # todo: get two pages for prev and next
-    return render(request, "book.html", {"page": page})
+    words = page.content.split(" ")
+    return render(
+        request,
+        "book.html",
+        {
+            "page": page,
+            "words": words,
+        },
+    )
 
 
 def book_page(request: HttpRequest) -> HttpResponse:
     """Render the book page."""
     page_number = request.GET.get("book-page", 1)
     try:
-        next_page = BookPage.objects.get(number=page_number)
+        page = BookPage.objects.get(number=page_number)
     except BookPage.DoesNotExist:
         return HttpResponse(f"error: Page {page_number} not found", status=500)
-
-    # Render only the content of the next page as a partial HTML response
+    last_word_id = request.GET.get("last-word-id", "0")
+    last_word_id = int(last_word_id) if last_word_id.isdigit() else 0
+    print("last_word_id", last_word_id)
+    words = page.content.split(" ")
     content = render_to_string(
-        "page.html",
+        "book.html",
         {
-            "page": next_page,
-            "type": request.GET.get("type"),
-            "book_page": request.GET.get("book-page"),
+            "page": page,
+            "words": words,
         },
     )
 
