6ee1ed13bbe84abe7c47f507c415a0a8306dffc3
andgineer <Andrei_Sorokin2@epam.com>
2024-08-08 18:07:06 +0200

    https://github.com/andgineer/lexiflux/issues/17 unapproved user e2e tests

--- tests/test_e2e_library_page.py
+++ tests/test_e2e_library_page.py
@@ -1,7 +1,33 @@
 import pytest
 import allure
+from django.urls import reverse
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from tests.conftest import USER_PASSWORD
 from tests.page_models.library_page import LibraryPage
+
+
+@allure.epic('End-to-end (selenium)')
+@allure.feature('Library Page')
+@allure.story('Un-approved user cannot access library page')
+@pytest.mark.docker
+@pytest.mark.selenium
+@pytest.mark.django_db
+def test_e2e_library_page_upapproved_user_cannot_access(browser, user):
+    browser.login(user, USER_PASSWORD, wait_view=None)
+    WebDriverWait(browser, 3).until(
+        EC.url_to_be(browser.host + reverse('login')),  # raise TimeoutException if not
+        message="Expected to be redirected to login page again after login with unapproved user.",
+    )
+    assert "Your account is not approved yet." in browser.errors_text
+
+    # just to be sure
+    allure.attach(
+        browser.get_screenshot_as_png(),
+        name='after_unapproved_login_screenshot',
+        attachment_type=allure.attachment_type.PNG
+    )
 
 
 @allure.epic('End-to-end (selenium)')
